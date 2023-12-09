9bd9a65da9c1e469407941dd89ec1bf9b2b8c80a
ksvaska <vas25ks@gmail.com>
2023-12-09 14:30:07 +0300

    c

--- w13.js
+++ w13.js
@@ -1,3 +1,4 @@
+
 const birthday = document.querySelector('#birthday');
 const button = document.querySelector('.btn');
 const result = document.querySelector('.result');
@@ -6,35 +7,33 @@ function a(){
 let day = new Date(birthday.value);
 let now = new Date();
 let er = Math.floor((day - now)/(1000*60*60*24));
+
 console.log(er);
 
+
+let x= 0;
+if(er<100){
+    x = er;
+} else if(er>=100 && er<200){
+   x= er-100;
+} else if (er >= 200 && er< 300) {
+  x = er - 200;
+  } else {
+    x = er- 300;
+  }
+console.log(x);
+ y = x % 10;
+ console.log(y);
+
+if(y===1 && x !==11){
+    result.textContent = `До дня рождения ${er} день.`;
+  } else if((y===2 && y===3 && y===4) && (x !==12 &&
+  x !== 13 &&
+  x !== 14)){
+   result.textContent = `До дня рождения ${er} дня.`;
+  } else {
+    result.textContent = `До дня рождения ${er} дней.`;
+  }
 }
-
-
-
-
-//let numBirth = 0;
-//if(birthday<100){
-    numBirth = birthday;
-//} else if(birthday>=100 && birthday<200){
-  //  numBirth = birthday-100;
-//} else if (birthday >= 200 && birthday < 300) {
-  ///  numBirth = birthday - 200;
- // } else (birthday >= 300) 
- // {
- //   numBirth = birthday - 300;
- // }
-
-
- // if(numBirth ===1 && numBirth !==11){
-    //result.textContent = `До дня рождения ${numBirth} день.`;
-  //} else if((numBirth===2 && numBirth===3 && numBirth===4) && numBirth !==12 &&
-  //numBirth !== 13 &&
-  // !== 14){
-  //  result.textContent = `До дня рождения ${numBirth} дня.`;
- // } else {
-  //  result.textContent = `До дня рождения ${numBirth} дней.`;
-  //}
-
 
   button.addEventListener('click',a);

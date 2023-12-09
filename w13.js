ae2c4dacfcf8d5095339b4e20a55c18b118feb20
ksvaska <vas25ks@gmail.com>
2023-12-09 21:07:13 +0300

    o

--- w13.js
+++ w13.js
@@ -6,33 +6,33 @@ const result = document.querySelector('.result');
 function a(){
 let day = new Date(birthday.value);
 let now = new Date();
-let er = Math.floor((day - now)/(1000*60*60*24));
+let diff = Math.floor((day - now)/(1000*60*60*24));
 
-console.log(er);
+console.log(diff);
 
 
-let x= 0;
-if(er<100){
-    x = er;
-} else if(er>=100 && er<200){
+let x= 0; //стандартизированное число, чтобы легче было считать
+if(diff<100){
+    x = diff;
+} else if(diff>=100 && diff<200){
    x= er-100;
-} else if (er >= 200 && er< 300) {
-  x = er - 200;
+} else if (diff >= 200 && diff< 300) {
+  x = diff - 200;
   } else {
-    x = er- 300;
+    x = diff- 300;
   }
 console.log(x);
- y = x % 10;
+ y = x % 10; //остаток деления для определния окончания 
  console.log(y);
 
 if(y===1 && x !==11){
-    result.textContent = `До дня рождения ${er} день.`;
+    result.textContent = `До дня рождения ${diff} день.`;
   } else if((y===2 && y===3 && y===4) && (x !==12 &&
   x !== 13 &&
   x !== 14)){
-   result.textContent = `До дня рождения ${er} дня.`;
+   result.textContent = `До дня рождения ${diff} дня.`;
   } else {
-    result.textContent = `До дня рождения ${er} дней.`;
+    result.textContent = `До дня рождения ${diff} дней.`;
   }
 }
 
